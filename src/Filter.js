3e2dde056e5c17facac80afee1f41df0d20b0bb1
iadam2000 <ibby200@hotmail.co.uk>
2024-10-18 06:52:57 +0300

    Styling for light/default mode complete

--- src/Filter.js
+++ src/Filter.js
@@ -1,11 +1,10 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
+import './css/Filter.css';
 
 function Filter({ topics }) {
-
     const [selectedTopic, setSelectedTopic] = useState("");
     const [selectedOrder, setSelectedOrder] = useState("created_at-desc");
-
     const navigate = useNavigate();
 
     const handleSelectChangeTopic = (event) => {
@@ -16,41 +15,40 @@ function Filter({ topics }) {
     };
 
     const handleSelectChangeOrder = (event) => {
-
         const sortBy = event.target.value.split("-")[0];
         const order = event.target.value.split("-")[1];
 
         if (event.target.value) {
-                navigate(`/articles?topic=${selectedTopic}&order=${order}&sort_by=${sortBy}`);
+            navigate(`/articles?topic=${selectedTopic}&order=${order}&sort_by=${sortBy}`);
         }
     };
 
     return (
         <div className="topic-selector">
-
             <label htmlFor="topics"></label>
-
-            <select id="topics" name="topics" value={selectedTopic} onChange={handleSelectChangeTopic}>
-                <option value="">Topic</option>
-                {topics.map((topic) => (
-                    <option key={topic.slug} value={topic.slug}>
-                        {topic.description}
-                    </option>
-                ))}
-            </select>
+            <div className="select-wrapper">
+                <select id="topics" name="topics" value={selectedTopic} onChange={handleSelectChangeTopic}>
+                    <option value="">Topic</option>
+                    {topics.map((topic) => (
+                        <option key={topic.slug} value={topic.slug}>
+                            {topic.description}
+                        </option>
+                    ))}
+                </select>
+            </div>
 
             <label htmlFor="order"></label>
-
-            <select id="order" name="order" value={selectedOrder} onChange={handleSelectChangeOrder}>
-                <option value={selectedOrder}>Sort By</option>
-                <option value="votes-asc">Votes ↑</option>
-                <option value="votes-desc">Votes ↓</option>
-                <option value="created_at-asc">Date ↑</option>
-                <option value="created_at-desc">Date ↓</option>
-                <option value="comment_count-asc">Comment Count ↑</option>
-                <option value="comment_count-desc">Comment Count ↓</option>
-            </select>
-
+            <div className="select-wrapper">
+                <select id="order" name="order" value={selectedOrder} onChange={handleSelectChangeOrder}>
+                    <option value={selectedOrder}>Sort By</option>
+                    <option value="votes-asc">Votes ↑</option>
+                    <option value="votes-desc">Votes ↓</option>
+                    <option value="created_at-asc">Date ↑</option>
+                    <option value="created_at-desc">Date ↓</option>
+                    <option value="comment_count-asc">Comment Count ↑</option>
+                    <option value="comment_count-desc">Comment Count ↓</option>
+                </select>
+            </div>
         </div>
     );
 }
--- src/SingleArticle.js
+++ src/SingleArticle.js
@@ -10,7 +10,7 @@ const SingleArticle = () => {
 
     const { id } = useParams();
     const { data, error, isLoading } = useFetch(`/articles/${id}`);
-    const article = data.article
+    const article = data.article;
 
     return (
         <div className="Single-article">
@@ -18,22 +18,23 @@ const SingleArticle = () => {
             {error && <div>{error}</div>}
             {data.article && (
                 <div className='single-article-view'>
-                    <Navbar/>
-                    <h2>{article.title}</h2>
-                    <p>{article.body}</p>
-                    <img src={article.article_img_url} alt={article.title} />
-                    <div className="article-details">
-                        <p><strong>Author:</strong> {article.author}</p>
-                        <p><strong>Topic:</strong> {article.topic}</p>
-                        <p><strong>Created at:</strong> {new Date(article.created_at).toLocaleDateString()}</p>
-                    </div>
-                    <Vote id={article.article_id} initialVotes={article.votes} />
+                    <Navbar />
+                    <div className="content">
+                        <h2>{article.title}</h2>
+                        <p>{article.body}</p>
+                        <img src={article.article_img_url} alt={article.title} />
+                        <div className="article-details">
+                            <p><strong>Author:</strong> {article.author}</p>
+                            <p><strong>Topic:</strong> {article.topic}</p>
+                            <p><strong>Created at:</strong> {new Date(article.created_at).toLocaleDateString()}</p>
+                        </div>
+                        <Vote id={article.article_id} initialVotes={article.votes} />
 
-                    <div className='comments-larger-container'>
-                    <Comments id={article.article_id} />
-                    <PostComment id={article.article_id}/>
+                        <div className='comments-larger-container'>
+                            <Comments id={article.article_id} />
+                            <PostComment id={article.article_id} />
+                        </div>
                     </div>
-
                 </div>)}
         </div>
     );
