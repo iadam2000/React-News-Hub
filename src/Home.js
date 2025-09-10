d6dcb6fe6fca64c3f7bd8f51557a83fc2248b184
iadam2000 <72634698+iadam2000@users.noreply.github.com>
2025-09-10 14:13:06 +0100

    Update Home.js

--- src/Home.js
+++ src/Home.js
@@ -2,16 +2,17 @@ import './css/index.css';
 import ArticleList from './ArticleList';
 import useFetch from './useFetch';
 import Navbar from './Navbar';
+import './css/spinner.css'; // Import the new spinner CSS
 
 const Home = () => {
-
     const { isLoading, data, error } = useFetch('/articles');
     
+    // Conditionally render the spinner
     if (isLoading) {
         return (
-            <p className='spinner'>
-                Loading
-            </p>
+            <div className='spinner-container'>
+                <div className='spinner'></div>
+            </div>
         );
     }
 
